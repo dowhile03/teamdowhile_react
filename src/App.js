a96f8cf339206dfe2bcb41fdc8e7f4159771209a
Vipul Sharma <sharmavipul9756@gmail.com>
2021-06-22 15:50:14 +0530

    react teamdowhile

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Nav from "./components/Nav/Nav";
+
+const App = () => {
+  return (
+    <div className="App">
+    <Nav/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { AppBar, Toolbar, IconButton, Typography, Button  } from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu';
+import classes from "./Nav.module.css"
+
+const Nav = () => {
+    return (
+        <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start"  color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6">
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    )
+}
+
+export default Nav
